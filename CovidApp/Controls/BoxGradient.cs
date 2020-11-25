ba834ce4b9098f934b26e8e0860e7899d921229d
Michal Svoboda <michal.svoboda@teliacompany.com>
2020-11-25 09:55:02 +0100

    Implement boxgradient component.

--- CovidApp/Controls/BoxGradient.cs
+++ CovidApp/Controls/BoxGradient.cs
@@ -10,50 +10,71 @@ namespace CovidApp.Controls
 {
     public class BoxGradient : SKCanvasView
     {
-        public static readonly BindableProperty CircleColorProperty = BindableProperty.Create(nameof(CircleColor),
-   typeof(Color), typeof(BoxGradient), Color.Blue,
-   propertyChanging: (currentControl, oldValue, newValue) =>
-   {
-       var thisControl = currentControl as BoxGradient;
-       thisControl.CircleColor = (Color)newValue;
-   });
+        public static readonly BindableProperty Color1Property = BindableProperty.Create(nameof(Color1),
+        typeof(Color), typeof(BoxGradient), Color.Blue,
+        propertyChanging: (currentControl, oldValue, newValue) =>
+        {
+            var thisControl = currentControl as BoxGradient;
+            thisControl.Color1 = (Color)newValue;
+        });
 
-        public static readonly BindableProperty CircleBackgroundColorProperty = BindableProperty.Create(nameof(CircleBackgroundColor),
-            typeof(Color), typeof(BoxGradient), Color.LightGray,
-            propertyChanging: (currentControl, oldValue, newValue) =>
-            {
-                var thisControl = currentControl as BoxGradient;
-                thisControl.CircleBackgroundColor = (Color)newValue;
-            });
+        public static readonly BindableProperty Color2Property = BindableProperty.Create(nameof(Color2),
+        typeof(Color), typeof(BoxGradient), Color.Blue,
+        propertyChanging: (currentControl, oldValue, newValue) =>
+        {
+            var thisControl = currentControl as BoxGradient;
+            thisControl.Color2 = (Color)newValue;
+        });
 
         public static readonly BindableProperty CircleStrokeWidthProperty = BindableProperty.Create(nameof(CircleStrokeWidth),
-            typeof(float), typeof(BoxGradient), (float)4,
-            propertyChanging: (currentControl, oldValue, newValue) =>
-            {
-                var thisControl = currentControl as BoxGradient;
-                thisControl.CircleStrokeWidth = (float)newValue;
-            });
+        typeof(float), typeof(BoxGradient), (float)4,
+        propertyChanging: (currentControl, oldValue, newValue) =>
+        {
+            var thisControl = currentControl as BoxGradient;
+            thisControl.CircleStrokeWidth = (float)newValue;
+        });
 
-        private SKColor myCircleColor;
-        private SKColor myCircleBackgroundColor;
+        public static readonly BindableProperty RoundRadiusProperty = BindableProperty.Create(nameof(RoundRadius),
+    typeof(float), typeof(BoxGradient), (float)4,
+    propertyChanging: (currentControl, oldValue, newValue) =>
+    {
+        var thisControl = currentControl as BoxGradient;
+        thisControl.myRoundRadius = (float)newValue;
+    });
+
+
+        private SKColor myColor1;
+        private SKColor myColor2;
         private SKColor myBackgroundColor;
         private float myCircleStrokeWidth;
+        private float myRoundRadius;
 
         public BoxGradient()
         {
             BackgroundColor = myBackgroundColor.ToFormsColor();
-            CircleBackgroundColor =  Color.LightGray;
-            CircleColor = Color.Blue;
+            Color1 = Color.Blue;
+            Color2 = Color.Blue;
             CircleStrokeWidth = 40;
         }
 
-        public Color CircleColor
+        public Color Color1
         {
-            get { return (Color)GetValue(CircleColorProperty); }
+            get { return (Color)GetValue(Color1Property); }
             set
             {
-                SetValue(CircleColorProperty, value);
-                myCircleColor = value.ToSKColor();
+                SetValue(Color1Property, value);
+                myColor1 = value.ToSKColor();
+                InvalidateSurface();
+            }
+        }
+
+        public Color Color2
+        {
+            get { return (Color)GetValue(Color2Property); }
+            set
+            {
+                SetValue(Color2Property, value);
+                myColor2 = value.ToSKColor();
                 InvalidateSurface();
             }
         }
@@ -69,16 +90,17 @@ namespace CovidApp.Controls
             }
         }
 
-        public Color CircleBackgroundColor
+        public float RoundRadius
         {
-            get { return (Color)GetValue(CircleBackgroundColorProperty); }
+            get { return (float)GetValue(CircleStrokeWidthProperty); }
             set
             {
-                SetValue(CircleBackgroundColorProperty, value);
-                myCircleBackgroundColor = value.ToSKColor();
+                SetValue(CircleStrokeWidthProperty, value);
+                myRoundRadius = value;
                 InvalidateSurface();
             }
         }
+
 
         protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
@@ -93,8 +115,7 @@ namespace CovidApp.Controls
         {
             base.OnPaintSurface(e);
 
-            SKRoundRect skRoundRect= new SKRoundRect(e.Info.Rect, 4);
-            SKImageInfo info = e.Info;
+            SKRoundRect skRoundRect= new SKRoundRect(e.Info.Rect, myRoundRadius);
             SKSurface surface = e.Surface;
             SKCanvas canvas = surface.Canvas;
 
@@ -109,12 +130,12 @@ namespace CovidApp.Controls
             };
 
             paint.Style = SKPaintStyle.Fill;
-            paint.Color = myCircleColor;
+            paint.Color = myColor1;
 
             paint.Shader = SKShader.CreateLinearGradient(
                     new SKPoint(skRoundRect.Rect.Left, skRoundRect.Rect.Top),
                     new SKPoint(skRoundRect.Rect.Right, skRoundRect.Rect.Bottom),
-                    new SKColor[] { SKColors.Red, SKColors.Orange },
+                    new SKColor[] { myColor1, myColor2 },
                     new float[] { 0, 1f },
                     SKShaderTileMode.Repeat);
 
